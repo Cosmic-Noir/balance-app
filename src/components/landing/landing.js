b00f7bcb8a1d696153f003eaa54103cc9bb1eb1f
Kristen Doucette <cosmicnoir90@gmail.com>
2019-12-28 15:59:32 -0700

    Fix center of Nav

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -11,6 +11,10 @@ export default function Landing() {
         <h2>
           Personal budgeting made <span></span>
         </h2>
+
+        <a href="https://www.freepik.com/free-photos-vectors/background">
+          Background vector created by s.salvador - www.freepik.com
+        </a>
       </header>
     </div>
   );
