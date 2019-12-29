f921d582ee6f13f14c43933b89cd98a6015f8bd1
Kristen Doucette <cosmicnoir90@gmail.com>
2019-12-29 14:47:22 -0700

    Add viewSavings.js

---
+++ src/components/viewSavings/viewSavings.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+/* Context */
+import balanceContext from "../../balanceContext";
+
+class ViewSavings extends Component {
+  state = {};
+
+  static contextType = balanceContext;
+
+  render() {
+    return (
+      <div className="createBudget">
+        <h2>Savings Report:</h2>
+        {/* Savings report with graph */}
+      </div>
+    );
+  }
+}
+
+export default ViewSavings;
