4117ef3e3256eda5a3286e532bc5d2cf99c98f12
Kristen Doucette <cosmicnoir90@gmail.com>
2019-12-29 14:24:49 -0700

    Add nav to dashboard

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -3,6 +3,9 @@ import React, { Component } from "react";
 /* Context */
 import balanceContext from "../../balanceContext";
 
+/* Styling & Images */
+import "./dashboard.css";
+
 class Dashboard extends Component {
   static contextType = balanceContext;
 
@@ -10,6 +13,13 @@ class Dashboard extends Component {
     return (
       <div className="dashboard flex-column">
         <h2>Welcome {this.context.userInfo.username}</h2>
+        <div class="lGrey grid-container list">
+          <div class="block">"View Budgets</div>
+          <div class="block">Create Budget</div>
+
+          <div class="block">View Savings</div>
+          <div class="block">View Spending</div>
+        </div>
       </div>
     );
   }
