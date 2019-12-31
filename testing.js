c640d85a7dd8a7d20e49f3528f22a60172c87c19
CosmicNoir <cosmicnoir90@gmail.com>
2019-12-31 16:45:20 -0700

    Create testing.js to figure out functions

---
+++ testing.js
@@ -0,0 +1,16 @@
+// Seed Data
+import Data from "./src/data.js";
+
+// I need functions to :
+// - Search and return all charges for matching user_id and month_id
+// - Sort array of "charges" by due date
+// Then with matching charges correctly sorted
+// Return dependent components
+/* for (every charge in matching array) {
+    if (category !== income){
+        return <Charge />
+    } else if (category === 'income'){
+        return <Remainder />
+    }
+}
+*/
