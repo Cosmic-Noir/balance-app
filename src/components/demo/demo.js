4ab710586822897c3206d9b6c98d44829c8004c0
Kristen Doucette <cosmicnoir90@gmail.com>
2020-02-01 11:24:49 -0700

    Complete budget demo

--- src/components/demo/demo.js
+++ src/components/demo/demo.js
@@ -3,29 +3,114 @@ import "./demo.css";
 
 class Demo extends Component {
   state = {
-    phase: "phase_one"
+    phase: 1
+  };
+
+  increasePhase = () => {
+    this.setState({ phase: this.state.phase + 1 });
   };
 
   render() {
     return (
-      <div className="demo">
-        {this.state.phase === "phase_one" ? (
-          <div className="phase_one tip">
-            <h2>
+      <div>
+        {this.state.phase === 1 ? (
+          <div className="demo phase_one tip">
+            <h2 id="text_one">
               Welcome to your demo tour! This breif tutorial will show you how
               the app works.
             </h2>
-            <button className="main_button" id="phase_one" type="button">
+            <button
+              className="demo_button main_button"
+              id="phase_one"
+              onClick={this.increasePhase}
+              type="button"
+            >
               Next
             </button>
           </div>
         ) : (
           ""
         )}
-        {this.state.phase === "phase_two" ? "" : ""}
-        {this.state.phase === "phase_three" ? "" : ""}
-        {this.state.phase === "phase_four" ? "" : ""}
-        {this.state.phase === "phase_five" ? "" : ""}
+        {this.state.phase === 2 ? (
+          <div className="demo phase_two">
+            <h2 className="tip">
+              Select any existing budget from this drop down.
+            </h2>
+            <button
+              className="demo_button main_button"
+              onClick={this.increasePhase}
+              type="button"
+            >
+              Next
+            </button>
+          </div>
+        ) : (
+          ""
+        )}
+        {this.state.phase === 3 ? (
+          <div className="demo phase_three">
+            <h2 className="tip">
+              Add bills or Income by clicking these buttons. Note that
+              "One-Time" charges/income will not be imported to newly created
+              budgets.
+            </h2>
+            <button
+              className="demo_button main_button"
+              id="phase_three"
+              onClick={this.increasePhase}
+              type="button"
+            >
+              Next
+            </button>
+          </div>
+        ) : (
+          ""
+        )}
+        {this.state.phase === 4 ? (
+          <div className="demo phase_four">
+            <h2 className="tip">
+              Edit any charges or income by clicking this button
+            </h2>
+            <button
+              className="demo_button main_button"
+              onClick={this.increasePhase}
+              type="button"
+            >
+              Next
+            </button>
+          </div>
+        ) : (
+          ""
+        )}
+        {this.state.phase === 5 ? (
+          <div className="demo phase_five">
+            <h2 className="tip">Click here to view your Spending Report</h2>
+            <button
+              className="demo_button main_button"
+              onClick={this.increasePhase}
+              type="button"
+            >
+              Next
+            </button>
+          </div>
+        ) : (
+          ""
+        )}
+        {this.state.phase === 6 ? (
+          <div className="demo phase_one tip">
+            <h2>See how easy that was? Time to balance your budget!</h2>
+            <button
+              className="demo_button main_button"
+              onClick={this.increasePhase}
+              type="button"
+              id="phase_six"
+            >
+              Finish
+            </button>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     );
   }
