ae8da9f625a86ca6ef8a47db973b5a6e6da86de9
Kristen Doucette <cosmicnoir90@gmail.com>
2020-01-31 20:48:57 -0700

    Switch config back to hosted server

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  // API_ENDPOINT: `https://mysterious-sea-08728.herokuapp.com/api/`,
-  API_ENDPOINT: `http://localhost:8000/api/`,
+  API_ENDPOINT: `https://mysterious-sea-08728.herokuapp.com/api/`,
+  // API_ENDPOINT: `http://localhost:8000/api/`,
   TOKEN_KEY: "Balance-Auth-Token"
 };
