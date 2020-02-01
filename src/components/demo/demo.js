263090039e546f20f9c9ce9b643625aa90c1833c
Kristen Doucette <cosmicnoir90@gmail.com>
2020-02-01 10:42:03 -0700

    correctly center initial demo phase_one

--- src/components/demo/demo.js
+++ src/components/demo/demo.js
@@ -3,13 +3,29 @@ import "./demo.css";
 
 class Demo extends Component {
   state = {
-    phase: ""
+    phase: "phase_one"
   };
 
   render() {
     return (
       <div className="demo">
-        <h2>This is your demo tour!</h2>
+        {this.state.phase === "phase_one" ? (
+          <div className="phase_one tip">
+            <h2>
+              Welcome to your demo tour! This breif tutorial will show you how
+              the app works.
+            </h2>
+            <button className="main_button" id="phase_one" type="button">
+              Next
+            </button>
+          </div>
+        ) : (
+          ""
+        )}
+        {this.state.phase === "phase_two" ? "" : ""}
+        {this.state.phase === "phase_three" ? "" : ""}
+        {this.state.phase === "phase_four" ? "" : ""}
+        {this.state.phase === "phase_five" ? "" : ""}
       </div>
     );
   }
